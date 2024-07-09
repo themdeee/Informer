2e8554a428ffdc394ed541aeec89fced5cd7eeb3
themdeee <mde819710@gmail.com>
2024-07-09 10:42:09 +0800

    Add secret.h

--- include/config.h
+++ include/config.h
@@ -1,7 +1,11 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-// Informer Config
+// #include "secret.h"
+
+/*
+  Informer Config
+*/
 #define INFORMER_FIRMWARE_VERSION_MAJOR   2
 #define INFORMER_FIRMWARE_VERSION_MINOR   2
 #define INFORMER_FIRMWARE_VERSION_PATCH   1
@@ -12,15 +16,24 @@
 #define INFORMER_SWITCH_WORK              HIGH
 #define INFORMER_SWITCH_REST              LOW
 
-// WiFi Config
-#define USE_WIFI      true
+/*
+  WiFi Config
+*/
+#define USE_WIFI        true
 
 #if USE_WIFI
-  #define WIFI_SSID   ""
-  #define WIFI_PASS   ""
+  #ifndef WIFI_SSID
+    #define WIFI_SSID   ""
+  #endif
+  
+  #ifndef WIFI_PASS
+    #define WIFI_PASS   ""
+  #endif
 #endif
 
-// ETH Config
+/*
+  ETH Config
+*/
 #define USE_ETH               false
 
 #if USE_ETH
@@ -35,13 +48,15 @@
   #define ETH_PIN_RST         10
 #endif
 
-// LCD Config
-#define USE_LCD           false
+/*
+  LCD Config
+*/
+#define USE_LCD             false
 
-#define LCD_PIN_BLK       48
+#define LCD_PIN_BLK         48
 
-#define LCD_STATE_WORK    HIGH
-#define LCD_STATE_REST    LOW
+#define LCD_STATE_WORK      HIGH
+#define LCD_STATE_REST      LOW
 
 #if USE_LCD
   #define LCD_PIN_MOSI      1
@@ -54,7 +69,9 @@
   #define LCD_PIN_INT       45
 #endif
 
-// Modem Config
+/*
+  Modem Config
+*/
 #define USE_MODEM           false
 
 #define MODEM_PIN_SWITCH    8
@@ -72,30 +89,51 @@
   #define MODEM_PIN_RST     15
 #endif
 
-// Blynk Config
-#define USE_BLYNK               true
+/*
+  Blynk Config
+*/
+#define USE_BLYNK                 true
 
 #if USE_BLYNK
-  #define BLYNK_TEMPLATE_ID     ""
-  #define BLYNK_TEMPLATE_NAME   ""
-  #define BLYNK_AUTH_TOKEN      ""
+  #ifndef BLYNK_TEMPLATE_ID
+    #define BLYNK_TEMPLATE_ID     ""
+  #endif
+  
+  #ifndef BLYNK_TEMPLATE_NAME
+    #define BLYNK_TEMPLATE_NAME   ""
+  #endif
+  
+  #ifndef BLYNK_AUTH_TOKEN
+    #define BLYNK_AUTH_TOKEN      ""
+  #endif
 #endif
 
-// Web Serial Config
-#define USE_WEB_SERIAL            true
+/*
+  Web Serial Config
+*/
+#define USE_WEB_SERIAL              true
 
 #if USE_WEB_SERIAL
-  #define WEB_SERIAL_PORT         80
+  #ifndef WEB_SERIAL_PORT
+    #define WEB_SERIAL_PORT         80
+  #endif
 
-  #define USE_WEB_SERIAL_AUTH     true
+  #define USE_WEB_SERIAL_AUTH       false
 #endif
 
 #if USE_WEB_SERIAL_AUTH
-  #define WEB_SERIAL_AUTH_USER    ""
-  #define WEB_SERIAL_AUTH_PASS    ""
+  #ifndef WEB_SERIAL_AUTH_USER
+    #define WEB_SERIAL_AUTH_USER    ""
+  #endif
+
+  #ifndef WEB_SERIAL_AUTH_PASS
+    #define WEB_SERIAL_AUTH_PASS    ""
+  #endif
 #endif
 
-// NTP Config
+/*
+  NTP Config
+*/
 #define USE_NTP           true
 
 #if USE_NTP
@@ -104,19 +142,30 @@
   #define NTP_DST         0
 #endif
 
-// Remoter Config
-#define USE_REMOTER             true
+/*
+  Remoter Config
+*/
+#define USE_REMOTER               true
 
 #if USE_REMOTER
-  #define REMOTER_SERVER_IP     "192.168.1.1"
-  #define REMOTER_SERVER_PORT   12345
+  #ifndef REMOTER_SERVER_IP
+    #define REMOTER_SERVER_IP     "192.168.1.1"
+  #endif
+
+  #ifndef REMOTER_SERVER_PORT
+    #define REMOTER_SERVER_PORT   12345  
+  #endif
 #endif
 
-// WOL Config
-#define USE_WOL     true
+/*
+  WOL Config
+*/
+#define USE_WOL       true
 
 #if USE_WOL
-  #define WOL_MAC   "01:23:45:67:89:AB"
+  #ifndef WOL_MAC
+    #define WOL_MAC   "01:23:45:67:89:AB"
+  #endif
 #endif
 
 #endif
